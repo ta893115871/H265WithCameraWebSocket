bef99c9dc33443fd5a5e8c682c277a3b400e863a
guxiuzhong <guxiuzhong@baidu.com>
2021-01-26 09:39:14 +0800

    remove log

--- app/src/main/java/com/bj/gxz/h265withcamerawebsocket/codec/EncodeH265.kt
+++ app/src/main/java/com/bj/gxz/h265withcamerawebsocket/codec/EncodeH265.kt
@@ -88,63 +88,8 @@ class EncodeH265 {
         }
 
     }
-    fun writeContent(array: ByteArray): String? {
-        val HEX_CHAR_TABLE = charArrayOf(
-            '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
-        )
-        val sb = StringBuilder()
-        for (b in array) {
-            sb.append(HEX_CHAR_TABLE[(b and 0x0f).toInt() shr 4])
-            sb.append(HEX_CHAR_TABLE[(b and 0x0f).toInt()])
-        }
-        Log.i(TAG, "writeContent: $sb")
-        var writer: FileWriter? = null
-        try {
-            // 打开一个写文件器，构造函数中的第二个参数true表示以追加形式写文件
-            writer = FileWriter(
-                Environment.getExternalStorageDirectory().toString() + "/codec.txt",
-                true
-            )
-            writer.write(sb.toString())
-            writer.write("\n")
-        } catch (e: IOException) {
-            e.printStackTrace()
-        } finally {
-            try {
-                if (writer != null) {
-                    writer.close()
-                }
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-        return sb.toString()
-    }
-    fun writeBytes(array: ByteArray?) {
-        var writer: FileOutputStream? = null
-        try {
-            // 打开一个写文件器，构造函数中的第二个参数true表示以追加形式写文件
-            writer = FileOutputStream(
-                Environment.getExternalStorageDirectory().toString() + "/codec.h264", true
-            )
-            writer.write(array)
-        } catch (e: IOException) {
-            e.printStackTrace()
-        } finally {
-            try {
-                if (writer != null) {
-                    writer.close()
-                }
-            } catch (e: IOException) {
-                e.printStackTrace()
-            }
-        }
-    }
+
     private fun dealFrame(byteBuffer: ByteBuffer) {
-//        val bytes = ByteArray(info.size)
-//        byteBuffer.get(bytes)
-//        writeContent(bytes)
-//        return
         // H265的nalu的分割符的下一个字节的类型
         var offset = 4
         if (byteBuffer[2].toInt() == 0x1) {
